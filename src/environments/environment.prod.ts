cb1e637dac01d7155d5f29021e233c6f33b4d06a
Kesava Raj <kesavarajtry@gmail.com>
2024-03-03 11:38:16 +0530

    new heroku ac added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
    //apiUrl: 'http://localhost:5000/api/',
-  apiUrl: 'https://pc-billbook.herokuapp.com/api/',
+  apiUrl: 'https://pcbillbook-8c619b3b6bad.herokuapp.com/api/',
 };
