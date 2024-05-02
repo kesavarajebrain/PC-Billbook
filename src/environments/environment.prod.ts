dcdf27ad7285e08a03756021db0d6766a84dc5fe
Kesava Raj <kesavarajtry@gmail.com>
2024-05-02 13:30:56 +0530

    api url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-   apiUrl: 'http://localhost:5000/api/',
-  //apiUrl: 'https://pcbillbook-8c619b3b6bad.herokuapp.com/api/',
+   //apiUrl: 'http://localhost:5000/api/',
+   apiUrl: 'https://pc-node-b0bfbc61ec31.herokuapp.com/api/',
 };
